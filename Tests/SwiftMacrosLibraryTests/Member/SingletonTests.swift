e35e0badf1c5027d67ca9de56a4532a464f2db48
kaijin <kaijin@sohu-inc.com>
2023-11-15 14:05:52 +0800

    add tests

--- Tests/SwiftMacrosLibraryTests/Member/SingletonTests.swift
+++ Tests/SwiftMacrosLibraryTests/Member/SingletonTests.swift
@@ -3,17 +3,17 @@ import SwiftSyntaxMacrosTestSupport
 import XCTest
 
 // Macro implementations build for the host, so the corresponding module is not available when cross-compiling. Cross-compiled tests may still make use of the macro itself in end-to-end tests.
-#if canImport(SwiftMacrosLibraryMacros)
-import SwiftMacrosLibraryMacros
+#if canImport(MacrosImplementation)
+import MacrosImplementation
 
 let testMacros: [String: Macro.Type] = [
-    "stringify": StringifyMacro.self,
+    "Singleton": Singleton.self,
 ]
 #endif
 
-final class SwiftMacrosLibraryTests: XCTestCase {
+final class SingletonTests: XCTestCase {
     func testMacro() throws {
-        #if canImport(SwiftMacrosLibraryMacros)
+        #if canImport(SingletonMacros)
         assertMacroExpansion(
             """
             #stringify(a + b)
@@ -29,7 +29,7 @@ final class SwiftMacrosLibraryTests: XCTestCase {
     }
 
     func testMacroWithStringLiteral() throws {
-        #if canImport(SwiftMacrosLibraryMacros)
+        #if canImport(SingletonMacros)
         assertMacroExpansion(
             #"""
             #stringify("Hello, \(name)")
@@ -43,4 +43,46 @@ final class SwiftMacrosLibraryTests: XCTestCase {
         throw XCTSkip("macros are only supported when running tests for the host platform")
         #endif
     }
+    
+    func testSingletonMacro() {
+        assertMacroExpansion(
+            """
+            @Singleton
+            struct A {
+            }
+            """,
+            expandedSource: """
+            struct A {
+            
+                private init() {
+                }
+            
+                static let shared = A()
+            }
+            """,
+            macros: testMacros
+        )
+    }
+
+    func testPublicSingletonMacro() {
+        assertMacroExpansion(
+            """
+            @Singleton
+            public struct A {
+            }
+            """,
+            expandedSource: """
+
+            public struct A {
+            
+                private init() {
+                }
+            
+                public static let shared = A()
+            }
+            """,
+            macros: testMacros
+        )
+    }
+    
 }
